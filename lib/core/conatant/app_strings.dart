e5547449d4342222113e2f6f54cb27401b98d9d4
Nora Salah <nora88n3@gmail.com>
2024-10-19 09:32:52 +0300

    modify

---
+++ lib/core/conatant/app_strings.dart
@@ -0,0 +1,13 @@
+class AssetStrings{
+  static String enterYourEmail="Enter Your Email";
+  static String logInToYourAccount="Log In To Your Account";
+  static String email="Email";
+  static String password="Password";
+  static String enterYourPassword="Enter Your Password";
+  static String dontHaveAnAccount = "Don’t have an account ? ";
+  static String logIn="Log In";
+  static String registerHere="Register Here";
+
+
+
+}
